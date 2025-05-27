1627b57a374c207068aa3d59036837e1fd17612a
Nilay Bhaisare <nilaybhaisare1098@gmail.com>
2025-05-27 14:56:56 +0100

    Add POST, PUT and DELETE endpoints for Task entity with Postman tests (#1)

--- src/main/java/com/myJourney/taskManagementAPI/controller/TaskController.java
+++ src/main/java/com/myJourney/taskManagementAPI/controller/TaskController.java
@@ -3,10 +3,7 @@ package com.myJourney.taskManagementAPI.controller;
 import com.myJourney.taskManagementAPI.model.Task;
 
 import com.myJourney.taskManagementAPI.service.TaskService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +26,26 @@ public class TaskController {
     @GetMapping( "/task/{id}")
     public Task getTaskById(@PathVariable Long id) {
         return service.getTaskById(id);
+    }
+
+    @PostMapping("/task")
+    public void addTask(@RequestBody Task task) {
+        service.addTask(task);
+    }
+
+    @PutMapping("/task/{id}")
+    public void updateTask(@PathVariable Long id, @RequestBody Task task) {
+        Task task1 = service.getTaskById(id);
+        if (task1 != null) {
+            task.setId(id);
+            service.updateTask(task);
+        }
+    }
+
+    @DeleteMapping("/task/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        Task task = service.getTaskById(id);
+        if (task != null) service.deleteTask(id);
     }
 
 }
